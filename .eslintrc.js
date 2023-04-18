47a49d9a76c4ad45aa943e2ba49048262f4a3dfb
Daria Nikulina <daryanickulina@yandex.ru>
2023-04-19 02:06:40 +0400

    feat: esLint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,13 @@ module.exports = {
     es2021: true,
   },
   extends: 'airbnb-base',
-  overrides: [
-  ],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   rules: {
+    'no-param-reassign': 0,
+    'operator-linebreak': ['error', 'after'],
   },
 };
